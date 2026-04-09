29d26783883e8017a5aa2ea1704b683cfc379d6a
Jay2002-creator <206188118+Jay-creator-200@users.noreply.github.com>
2026-04-10 01:07:03 +0530

    update

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Frame from "../components/Frame";
+import FrameStudio from "@/components/FrameStudio";
 
 export default function Home() {
   return (
     <div>
-      <Frame/>
+      <FrameStudio/>
     </div>
   );
 }
